7219a090b81179917c1f33b061c04f5471398acc
Joey Cozza <joeycozza@gmail.com>
2015-01-21 16:25:19 -0700

    Initial Comit

---
+++ index.js
@@ -0,0 +1,30 @@
+var traverse    = require('traverse');
+var dottie      = require('dottie');
+var _ 		= require('lodash');
+
+exports.module = function transformJSONToTable(docs) {
+  // Go through each object, find the deepest path
+  // Creat an array of all of the possible paths
+  var headers = _.keys(traverse(docs).reduce(
+                      function(headers, value) {
+                        if (this.isLeaf) {
+                          // Make sure that we don't include empty objects as valid headers
+                          // There are good reasons behind this that deal with projections, and non-homogenous objects in a collection.
+                          if (!(_.isPlainObject(value) && _.keys(value).length === 0)) { // Check against empty objects. Don't treat these paths as a valid header value.
+                            headers[_.rest(this.path).join('.')] = true;
+                          }
+                        }
+                        return headers;
+                      }, {})
+                );
+  // Go through each object again, this time, attempt to grab the value
+  // At each possible path.
+  var data = [headers];
+  data = data.concat(_.map(docs, function(doc) {
+                return _.map(headers, function(header) {
+                  return dottie.get(doc, header, '');
+                })
+              }));
+
+  return data;
+};
