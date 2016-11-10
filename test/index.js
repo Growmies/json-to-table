756b889fbb4be2b23fafbef31b895a0045797484
Joey Cozza <joeycozza@gmail.com>
2016-11-10 11:45:22 -0700

    fixed issue with keeping arrays on edge cases even if excludeSubArrays was true

--- test/index.js
+++ test/index.js
@@ -3,9 +3,9 @@ var assert      = require('chai').assert,
     _           = require('lodash'),
     jsonToTable = require('../');
 
-describe('Basic usage', function() {
+describe('Basic usage', function () {
 
-  it('Should take a simple array of JSON objects and create a simple table', function() {
+  it('Should take a simple array of JSON objects and create a simple table', function () {
     var jsonData = [
       {
         firstName: 'Scott',
@@ -27,21 +27,21 @@ describe('Basic usage', function() {
       }
     ];
 
-    var tableData = jsonToTable(jsonData);
-    var expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email' ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com' ],
-                              [ 'Burt',      'Macklin',  '801-555-5555', 'burt@fbi.gov' ],
-                              [ 'Ron',       'Swanson',  '801-555-5555', 'ron@pawnee.gov' ] ];
+    var tableData         = jsonToTable(jsonData);
+    var expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email'],
+      ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com'],
+      ['Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov'],
+      ['Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov']];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should take a array of JSON object with nested properties and create a table', function() {
+  it('Should take a array of JSON object with nested properties and create a table', function () {
     var jsonData = [
       {
         id: 1,
         occupation: 'web developer',
-        contact : {
+        contact: {
           firstName: 'Scott',
           lastName: 'Hillman',
           phoneNumber: '801-555-5555',
@@ -56,7 +56,7 @@ describe('Basic usage', function() {
           lastName: 'Macklin',
           phoneNumber: '801-555-5555',
           email: 'burt@fbi.gov'
-        },
+        }
       },
       {
         id: 3,
@@ -71,20 +71,20 @@ describe('Basic usage', function() {
     ];
 
     var tableData         = jsonToTable(jsonData),
-        expectedTableData = [ [ 'id', 'occupation',          'contact.firstName', 'contact.lastName', 'contact.phoneNumber', 'contact.email' ],
-                              [ 1,    'web developer',       'Scott',             'Hillman',          '801-555-5555',        'scott@grow.com' ],
-                              [ 2,    'fbi agent',           'Burt',              'Macklin',          '801-555-5555',        'burt@fbi.gov' ],
-                              [ 3,    'government employee', 'Ron',               'Swanson',          '801-555-5555',        'ron@pawnee.gov' ] ]
+        expectedTableData = [['id', 'occupation', 'contact.firstName', 'contact.lastName', 'contact.phoneNumber', 'contact.email'],
+          [1, 'web developer', 'Scott', 'Hillman', '801-555-5555', 'scott@grow.com'],
+          [2, 'fbi agent', 'Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov'],
+          [3, 'government employee', 'Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov']];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should take an array of non-homogenous JSON objects and create a table', function() {
+  it('Should take an array of non-homogenous JSON objects and create a table', function () {
     var jsonData = [
       {
         id: 1,
         occupation: 'web developer',
-        contact : {
+        contact: {
           firstName: 'Scott',
           lastName: 'Hillman',
           phoneNumber: '801-555-5555',
@@ -96,28 +96,28 @@ describe('Basic usage', function() {
         contact: {
           phoneNumber: '801-555-5555',
           email: 'burt@fbi.gov'
-        },
+        }
       },
       {
         occupation: 'government employee',
         contact: {
           firstName: 'Ron',
-          lastName: 'Swanson',
+          lastName: 'Swanson'
         }
       }
     ];
 
     var tableData         = jsonToTable(jsonData),
-        expectedTableData = [ [ 'id', 'occupation',          'contact.firstName', 'contact.lastName', 'contact.phoneNumber', 'contact.email' ],
-                              [ 1,    'web developer',       'Scott',             'Hillman',          '801-555-5555',        'scott@grow.com' ],
-                              [ 2,    '',                    '',                  '',                 '801-555-5555',        'burt@fbi.gov' ],
-                              [ '',   'government employee', 'Ron',               'Swanson',          '',                    '' ] ]
+        expectedTableData = [['id', 'occupation', 'contact.firstName', 'contact.lastName', 'contact.phoneNumber', 'contact.email'],
+          [1, 'web developer', 'Scott', 'Hillman', '801-555-5555', 'scott@grow.com'],
+          [2, '', '', '', '801-555-5555', 'burt@fbi.gov'],
+          ['', 'government employee', 'Ron', 'Swanson', '', '']];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should take an array with sub-arrays and include the length of that array', function() {
-     var jsonData = [
+  it('Should take an array with sub-arrays and include the length of that array', function () {
+    var jsonData = [
       {
         firstName: 'Scott',
         lastName: 'Hillman',
@@ -141,16 +141,16 @@ describe('Basic usage', function() {
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { includeCollectionLength: true });
-        expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email',          'friends.length', 'friends.0', 'friends.1', 'friends.2', 'friends.3', 'friends.4', 'friends' ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com', 5,                'Trent',     'Andrew',    'Joey',      'Ryan',      'Rob',       [ 'Trent',   'Andrew',     'Joey', 'Ryan', 'Rob' ] ],
-                              [ 'Burt',      'Macklin',  '801-555-5555', 'burt@fbi.gov',   2,                'April',     'Leslie',    '',          '',          '',          [ 'April',   'Leslie' ] ],
-                              [ 'Ron',       'Swanson',  '801-555-5555', 'ron@pawnee.gov', 0,                '',          '',          '',          '',          '',          [] ] ];
+    var tableData     = jsonToTable(jsonData, {includeCollectionLength: true});
+    expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email', 'friends.length', 'friends.0', 'friends.1', 'friends.2', 'friends.3', 'friends.4', 'friends'],
+      ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com', 5, 'Trent', 'Andrew', 'Joey', 'Ryan', 'Rob', ['Trent', 'Andrew', 'Joey', 'Ryan', 'Rob']],
+      ['Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov', 2, 'April', 'Leslie', '', '', '', ['April', 'Leslie']],
+      ['Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov', 0, '', '', '', '', '', []]];
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should take an array with sub-arrays and include the length of that array, but not include the array itself', function() {
-     var jsonData = [
+  it('Should take an array with sub-arrays and include the length of that array, but not include the array itself', function () {
+    var jsonData = [
       {
         firstName: 'Scott',
         lastName: 'Hillman',
@@ -174,17 +174,17 @@ describe('Basic usage', function() {
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { includeCollectionLength: true, excludeSubArrays: true }),
-        expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email',          'friends.length' ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com', 5 ],
-                              [ 'Burt',      'Macklin',  '801-555-5555', 'burt@fbi.gov',   2 ],
-                              [ 'Ron',       'Swanson',  '801-555-5555', 'ron@pawnee.gov', 1 ] ];
+    var tableData         = jsonToTable(jsonData, {includeCollectionLength: true, excludeSubArrays: true}),
+        expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email', 'friends.length'],
+          ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com', 5],
+          ['Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov', 2],
+          ['Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov', 1]];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should properly handle a default value', function() {
-     var jsonData = [
+  it('Should properly handle a default value', function () {
+    var jsonData = [
       {
         firstName: 'Scott',
         lastName: 'Hillman',
@@ -196,27 +196,27 @@ describe('Basic usage', function() {
         firstName: 'Burt',
         lastName: 'Macklin',
         phoneNumber: '801-555-5555',
-        email: 'burt@fbi.gov',
+        email: 'burt@fbi.gov'
       },
       {
         firstName: 'Ron',
         lastName: 'Swanson',
         phoneNumber: '801-555-5555',
-        email: 'ron@pawnee.gov',
+        email: 'ron@pawnee.gov'
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { defaultVal: 'None' }),
-        expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email',          'title' ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com', 'Master runner' ],
-                              [ 'Burt',      'Macklin',  '801-555-5555', 'burt@fbi.gov',   'None' ],
-                              [ 'Ron',       'Swanson',  '801-555-5555', 'ron@pawnee.gov', 'None' ] ];
+    var tableData         = jsonToTable(jsonData, {defaultVal: 'None'}),
+        expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email', 'title'],
+          ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com', 'Master runner'],
+          ['Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov', 'None'],
+          ['Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov', 'None']];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should take undefined as a valid default value', function() {
-     var jsonData = [
+  it('Should take undefined as a valid default value', function () {
+    var jsonData = [
       {
         firstName: 'Scott',
         lastName: 'Hillman',
@@ -228,28 +228,28 @@ describe('Basic usage', function() {
         firstName: 'Burt',
         lastName: 'Macklin',
         phoneNumber: '801-555-5555',
-        email: 'burt@fbi.gov',
+        email: 'burt@fbi.gov'
       },
       {
         firstName: 'Ron',
         lastName: 'Swanson',
         phoneNumber: '801-555-5555',
-        email: 'ron@pawnee.gov',
+        email: 'ron@pawnee.gov'
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { defaultVal: undefined }),
-        expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email',          'title' ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com', 'Master runner' ],
-                              [ 'Burt',      'Macklin',  '801-555-5555', 'burt@fbi.gov',   undefined ],
-                              [ 'Ron',       'Swanson',  '801-555-5555', 'ron@pawnee.gov', undefined ] ];
+    var tableData         = jsonToTable(jsonData, {defaultVal: undefined}),
+        expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email', 'title'],
+          ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com', 'Master runner'],
+          ['Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov', undefined],
+          ['Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov', undefined]];
 
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should treat strings surround with tick marks as column name, not as a path', function() {
-     var jsonData = [
+  it('Should treat strings surround with tick marks as column name, not as a path', function () {
+    var jsonData = [
       {
         firstName: 'Scott',
         lastName: 'Hillman',
@@ -262,7 +262,7 @@ describe('Basic usage', function() {
         firstName: 'Burt',
         lastName: 'Macklin',
         phoneNumber: '801-555-5555',
-        email: 'burt@fbi.gov',
+        email: 'burt@fbi.gov'
       },
       {
         firstName: 'Ron',
@@ -273,34 +273,34 @@ describe('Basic usage', function() {
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { defaultVal: undefined, checkKeyBeforePath: true }),
-        expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email',          'title',         'friends.length' ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com', 'Master runner', 5 ],
-                              [ 'Burt',      'Macklin',  '801-555-5555', 'burt@fbi.gov',   undefined,       undefined ],
-                              [ 'Ron',       'Swanson',  '801-555-5555', 'ron@pawnee.gov', undefined,       6 ] ];
+    var tableData         = jsonToTable(jsonData, {defaultVal: undefined, checkKeyBeforePath: true}),
+        expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email', 'title', 'friends.length'],
+          ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com', 'Master runner', 5],
+          ['Burt', 'Macklin', '801-555-5555', 'burt@fbi.gov', undefined, undefined],
+          ['Ron', 'Swanson', '801-555-5555', 'ron@pawnee.gov', undefined, 6]];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should work as you would expect on a plain object', function() {
+  it('Should work as you would expect on a plain object', function () {
 
     var jsonData = {
       firstName: 'Scott',
       lastName: 'Hillman',
       phoneNumber: '801-555-5555',
       email: 'scott@grow.com',
-      title: 'Master runner',
+      title: 'Master runner'
     };
 
-    var tableData = jsonToTable(jsonData),
-        expectedTableData = [ [ 'firstName', 'lastName', 'phoneNumber',  'email',          'title',        ],
-                              [ 'Scott',     'Hillman',  '801-555-5555', 'scott@grow.com', 'Master runner' ]
-                            ];
+    var tableData         = jsonToTable(jsonData),
+        expectedTableData = [['firstName', 'lastName', 'phoneNumber', 'email', 'title'],
+          ['Scott', 'Hillman', '801-555-5555', 'scott@grow.com', 'Master runner']
+        ];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should be able to resolve the path or key even with multiple dots in the name.', function() {
+  it('Should be able to resolve the path or key even with multiple dots in the name.', function () {
     var jsonData = [
       {
         firstName: 'Scott',
@@ -316,15 +316,15 @@ describe('Basic usage', function() {
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { defaultVal: undefined, checkKeyBeforePath: true }),
-        expectedTableData = [ [ 'firstName', 'contact.`phone.number`', 'contact.`home.address`' ],
-                              [ 'Scott',     '801-999-8626',           undefined ],
-                              [ 'Burt',      undefined,                'The Pentagon' ] ];
+    var tableData         = jsonToTable(jsonData, {defaultVal: undefined, checkKeyBeforePath: true}),
+        expectedTableData = [['firstName', 'contact.`phone.number`', 'contact.`home.address`'],
+          ['Scott', '801-999-8626', undefined],
+          ['Burt', undefined, 'The Pentagon']];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
 
-  it('Should be able to resolve the path or key even with multiple dots in the name2.', function() {
+  it('Should be able to resolve the path or key even with multiple dots in the name2.', function () {
     var jsonData = [
       {
         firstName: 'Scott',
@@ -336,11 +336,102 @@ describe('Basic usage', function() {
       }
     ];
 
-    var tableData = jsonToTable(jsonData, { defaultVal: undefined, checkKeyBeforePath: true }),
-        expectedTableData = [ [ 'firstName', 'phone.number', '.ba`d' ],
-                              [ 'Scott',     '801-999-8626',           undefined ],
-                              [ 'Burt',      undefined,                'The Pentagon' ] ];
+    var tableData         = jsonToTable(jsonData, {defaultVal: undefined, checkKeyBeforePath: true}),
+        expectedTableData = [['firstName', 'phone.number', '.ba`d'],
+          ['Scott', '801-999-8626', undefined],
+          ['Burt', undefined, 'The Pentagon']];
 
     assert(_.isEqual(tableData, expectedTableData));
   });
+
+  it('Should handle when a nested array has data for 1 object, but is null (which means is a leaf) in another object', function () {
+    var jsonData = [
+      {
+        id: 1,
+        occupation: 'web developer',
+        contact: {
+          firstName: 'Scott',
+          lastName: 'Hillman',
+          phoneNumber: '801-555-5555',
+          email: 'scott@grow.com'
+        }
+      },
+      {
+        id: 2,
+        contact: {
+          phoneNumber: '801-555-5555',
+          email: 'burt@fbi.gov',
+          friends: ['joey']
+        }
+      },
+      {
+        occupation: 'government employee',
+        contact: {
+          firstName: 'Ron',
+          lastName: 'Swanson',
+          friends: null
+        }
+      }
+    ];
+
+    var tableData         = jsonToTable(jsonData, {excludeSubArrays: true}),
+        expectedTableData = [['id', 'occupation', 'contact.firstName', 'contact.lastName', 'contact.phoneNumber', 'contact.email', 'contact.friends'],
+          [1, 'web developer', 'Scott', 'Hillman', '801-555-5555', 'scott@grow.com', ''],
+          [2, '', '', '', '801-555-5555', 'burt@fbi.gov', ''],
+          ['', 'government employee', 'Ron', 'Swanson', '', '', null]];
+
+    assert(_.isEqual(tableData, expectedTableData));
+  });
+
+  it('Should handle when a nested array has data for 1 object, but is a primitive (which means is a leaf) in another object', function () {
+    var jsonData = [
+      {
+        id: 1,
+        friends: ['joey']
+      },
+      {
+        id: 2,
+        friends: true
+      }
+    ];
+
+    var tableData1         = jsonToTable(jsonData, {excludeSubArrays: true});
+    var expectedTableData1 = [['id', 'friends'], [1, ''], [2, true]];
+
+    var tableData2         = jsonToTable(jsonData);
+    var expectedTableData2 = [['id', 'friends.0', 'friends'],
+      [1, 'joey', ['joey']],
+      [2, '', true]];
+
+    assert(_.isEqual(tableData1, expectedTableData1));
+    assert(_.isEqual(tableData2, expectedTableData2));
+  });
+
+  it('Should handle when a nested object has data for 1 object, but is a primitive (which means is a leaf) in another object', function () {
+    var jsonData = [
+      {
+        id: 1,
+        friends: {name: 'joey'}
+      },
+      {
+        id: 2,
+        friends: 5
+      }
+    ];
+
+    var tableData1         = jsonToTable(jsonData, {excludeSubArrays: true});
+    var expectedTableData1 = [['id', 'friends.name', 'friends'],
+      [1, 'joey', {name: 'joey'}],
+      [2, '', 5]];
+
+    var tableData2         = jsonToTable(jsonData);
+    var expectedTableData2 = [['id', 'friends.name', 'friends'],
+      [1, 'joey', {name: 'joey'}],
+      [2, '', 5]];
+
+    assert(_.isEqual(tableData1, expectedTableData1));
+    assert(_.isEqual(tableData2, expectedTableData2));
+  });
+
+
 });
