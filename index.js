80cc68178a963a28d350fb6ff24fe677ffcef968
Joey Cozza <joeycozza@gmail.com>
2016-11-28 10:31:59 -0700

    more es6-ified

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var traverse = require('traverse');
-var _        = require('lodash');
+const traverse = require('traverse');
+const _        = require('lodash');
 
-module.exports = function transformJSONToTable(docs, options) {
-  options            = options || {};
+module.exports = function transformJSONToTable(docs, options = {}) {
   options.defaultVal = _.has(options, 'defaultVal') ? options.defaultVal : '';
 
   if (_.isPlainObject(docs)) {
@@ -11,21 +10,21 @@ module.exports = function transformJSONToTable(docs, options) {
 
   // Go through each object, find the deepest path
   // Create an array of all of the possible paths
-  var headers = _.keys(traverse(docs).reduce(
+  let allHeaders = _.keys(traverse(docs).reduce(
     function (headers, value) {
-      var self = this;
+      const self = this;
       if (this.notRoot && _.isArray(value)) {
         if (options.includeCollectionLength) {
-          headers[_.tail(this.path).join('.') + '.length'] = true;
+          headers[`${_.tail(this.path).join('.')}.length`] = true;
         }
         if (options.excludeSubArrays) {
           this.update(value, true);
         }
       }
       if (this.isLeaf) {
-        this.path = _.map(this.path, function (level) {
+        this.path = _.map(this.path, level => {
           if (level.indexOf('.') > -1 && self.level > 2) { // If a leaf contains a dot in it, then surround the whole path with ticks
-            level = '`' + level + '`';
+            level = `\`${level}\``;
           }
           return level;
         });
@@ -38,16 +37,16 @@ module.exports = function transformJSONToTable(docs, options) {
   );
   // Go through each object again, this time, attempt to grab the value
   // At each possible path.
-  var data = [headers];
-  data     = data.concat(_.map(docs, function (doc) {
-    return _.map(headers, function (header) {
+  let tableData = [allHeaders];
+  tableData     = tableData.concat(_.map(docs, doc => {
+    return _.map(allHeaders, header => {
       if (options.checkKeyBeforePath && doc[header]) {
         return doc[header];
       }
       if (header.indexOf('.`') > -1) { // One of those special cases where a path is nested, AND has a dot in the name.
-        var parts = header.split('.`');
-        var head  = parts[0].replace(/\`/g, '');
-        var tail  = parts[1].replace(/\`/g, '');
+        const parts = header.split('.`');
+        const head  = parts[0].replace(/\`/g, '');
+        const tail  = parts[1].replace(/\`/g, '');
 
         return _.get(doc, head, {})[tail];
       }
@@ -56,5 +55,5 @@ module.exports = function transformJSONToTable(docs, options) {
     });
   }));
 
-  return data;
+  return tableData;
 };
